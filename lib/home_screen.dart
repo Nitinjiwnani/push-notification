eecdf12a07c6068abd9ea86aa8c2ea30b13a8fa4
Nitinjiwnani <jiwnani01@gmail.com>
2023-03-13 17:13:37 +0530

    active state app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,6 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
     // TODO: implement initState
     super.initState();
     notificationServices.requestNotificationPermission();
+    notificationServices.firebaseInit(context);
     // notificationServices.isRefreshToken();
     notificationServices.getDeviceToken().then((token) {
       print('Device Token');
--- lib/notification_services.dart
+++ lib/notification_services.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationServices {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   void requestNotificationPermission() async {
     NotificationSettings settings = await messaging.requestPermission(
@@ -21,6 +28,64 @@ class NotificationServices {
     } else {
       print('user deneid permission');
     }
+  }
+
+  void initLocalNotifications(
+      BuildContext context, RemoteMessage message) async {
+    var androidInitializationSettings =
+        const AndroidInitializationSettings('ic_launcher');
+    var iosInitializationSettings = const DarwinInitializationSettings();
+    var initializationSetting = InitializationSettings(
+        android: androidInitializationSettings, iOS: iosInitializationSettings);
+    await _flutterLocalNotificationsPlugin.initialize(
+      initializationSetting,
+      onDidReceiveNotificationResponse: (payload) {},
+    );
+  }
+
+  void firebaseInit(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((message) {
+      print(message.notification!.title.toString());
+      print(message.notification!.body.toString());
+      showNotification(message);
+    });
+  }
+
+  Future<void> showNotification(RemoteMessage message) async {
+    AndroidNotificationChannel channel = AndroidNotificationChannel(
+        Random.secure().nextInt(100000).toString(),
+        'High Importance Notifications',
+        importance: Importance.max);
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      channel.id.toString(),
+      channel.name.toString(),
+      channelDescription: 'your channel description',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      icon: "@mipmap/ic_launcher",
+    );
+
+    const DarwinNotificationDetails darwinNotificationDetails =
+        DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
+    Future.delayed(
+      Duration.zero,
+      () {
+        _flutterLocalNotificationsPlugin.show(
+            0,
+            message.notification!.title.toString(),
+            message.notification!.body.toString(),
+            notificationDetails);
+      },
+    );
   }
 
   Future<String?> getDeviceToken() async {
