b0a2e3faba8f75fedadd660ca4cabd59397cc9de
Nitinjiwnani <jiwnani01@gmail.com>
2023-03-13 17:52:49 +0530

    noti for background state

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:push_noti_app/home_screen.dart';
+
+Future<void> _firebaseMessagingBackGroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print(message.notification!.title.toString());
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackGroundHandler);
   runApp(const MyApp());
 }
 
